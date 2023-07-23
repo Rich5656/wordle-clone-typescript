d8a32eebf631c730c0c9af9a6c3e9bb855addca8
Rich5656 <richard.k.lowther@gmail.com>
2023-07-23 13:27:03 -0400

    Styling and logic for absolute and relative matches added.

--- src/Components/BoardRow.tsx
+++ src/Components/BoardRow.tsx
@@ -2,13 +2,34 @@ import React from 'react'
 
 interface BoardRowProps {
     guess: string[];
+    submitted: boolean;
+    answer: string[];
 }
 
-export const BoardRow = ({ guess }: BoardRowProps) => {
+export const BoardRow = ({ guess, submitted, answer }: BoardRowProps) => {
+  const boardRowContent = (guess: string[], submitted: boolean) => {
+    if (submitted === false) {
+        return guess.map((letter) => <div className="tile"><p>{letter}</p></div>)
+    } else {
+        // TODO: fill in the logic for relative and absolute match styling
+        console.log('submitted')
+        return guess.map((letter, index) => {
+            if (answer[index] === letter) {
+                return <div className='tile absolute-match'><p>{letter}</p></div>
+            }
+            if (answer.includes(letter)) {
+                return <div className='tile relative-match'><p>{letter}</p></div>
+            }
+            return <div className='tile'><p>{letter}</p></div>
+        })
+    }
+  } 
+
   return (
     <div className="playing-board-row">
         {/* TODO: pass a tuple to props that can be mapped to these tiles */}
-        {guess.map(letter => <div className="tile"><p>{letter}</p></div>)}
+        {/* {guess.map(letter => <div className="tile"><p>{letter}</p></div>)} */}
+        {boardRowContent(guess, submitted)}
         {/* <div className="tile"><p>W</p></div>
         <div className="tile"><p>O</p></div>
         <div className="tile"><p>R</p></div>
--- src/Containers/PlayingBoardContainer.tsx
+++ src/Containers/PlayingBoardContainer.tsx
@@ -1,6 +1,7 @@
 import React, { useState, MouseEvent } from 'react';
 import { BoardRow } from '../Components/BoardRow';
 import { KeyboardDisplay } from '../Components/KeyboardDisplay';
+import { AssertionError } from 'assert';
 
 interface GuessSetterMap {
     [position: number]: React.Dispatch<React.SetStateAction<string[]>>;
@@ -9,11 +10,23 @@ interface GuessSetterMap {
 export const PlayingBoardContainer = () => {
   const answer: string[] = ['w', 'o', 'r', 'd', 's'];
   const [ firstGuess , setFirstGuess ] = useState<string[]>(['','','','','']);
+  const [ firstSubmitted, setFristSubmitted ] = useState<boolean>(false);
+
   const [ secondGuess , setSecondGuess ] = useState<string[]>(['','','','','']);
+  const [ secondSubmitted, setSecondSubmitted ] = useState<boolean>(false);
+
   const [ thirdGuess , setThirdGuess ] = useState<string[]>(['','','','','']);
+  const [ thirdSubmitted, setThirdSubmitted ] = useState<boolean>(false);
+
   const [ fourthGuess , setFourthGuess ] = useState<string[]>(['','','','','']);
+  const [ fourthSubmitted, setFourthSubmitted ] = useState<boolean>(false);
+
   const [ fifthGuess , setFifthGuess ] = useState<string[]>(['','','','','']);
+  const [ fifthSubmitted, setFifthSubmitted ] = useState<boolean>(false);
+
   const [ sixthGuess , setSixthGuess ] = useState<string[]>(['','','','','']);
+  const [ sixthSubmitted, setSixthSubmitted ] = useState<boolean>(false);
+
   const [ level, setLevel ] = useState<number>(1);
   console.log(level)
 
@@ -48,21 +61,32 @@ export const PlayingBoardContainer = () => {
   const handleCheck = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     // check that the length of the word is valid
+    for (const letter of firstGuess) {
+        if (letter === '') {
+            console.log('too short')
+            return;
+        } 
+    }
     // check that the word is a valid word
     // check if word mathces the key
-    // if all of these are false update the level to level 2 and apply needed styling to the tiles
+    if (firstGuess.join() === answer.join()) {
+        console.log('correct!');
+        return;
+    }
+    // if all of these are false update the level to level 2 and apply needed styling to the tiles based on absolute and relative match
     setLevel(prevLevel => prevLevel + 1);
+    setFristSubmitted(true);
   }
 
   return (
     <main>
         {/* TODO: boardrow will need to accept props */}
-        <BoardRow  guess={firstGuess} />
-        <BoardRow guess={secondGuess} />
-        <BoardRow guess={thirdGuess} />
-        <BoardRow guess={fourthGuess} />
-        <BoardRow guess={fifthGuess} />
-        <BoardRow guess={sixthGuess} />
+        <BoardRow  guess={firstGuess} submitted={firstSubmitted} answer={answer}/>
+        <BoardRow guess={secondGuess} submitted={secondSubmitted} answer={answer}/>
+        <BoardRow guess={thirdGuess} submitted={thirdSubmitted} answer={answer}/>
+        <BoardRow guess={fourthGuess} submitted={fourthSubmitted} answer={answer}/>
+        <BoardRow guess={fifthGuess} submitted={fifthSubmitted} answer={answer}/>
+        <BoardRow guess={sixthGuess} submitted={sixthSubmitted} answer={answer}/>
         <KeyboardDisplay handleKeyClick={handleKeyClick} handleCheck={handleCheck}/>
     </main>
   )
